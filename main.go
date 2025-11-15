d12a62e7ca15576b20da4e7d5ab2195a432f9ab9
arupanandaswain <arupaswain7735@gmail.com>
2025-11-15 11:08:30 +0530

    creating a directory

--- main.go
+++ main.go
@@ -12,4 +12,8 @@ func main(){
         return
     }
     fmt.Println("Created myDir successfully and the files are", files)
+    for _, file := range files{
+        fmt.Println(file.Name())
+        fmt.Println(file.IsDir())
+    }
 }
