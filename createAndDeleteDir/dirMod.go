c7cb1bef916ea06d3c7017e717a36d641aef7222
arupanandaswain <arupaswain7735@gmail.com>
2025-11-18 13:31:58 +0530

    updated main and dirMod files

--- createAndDeleteDir/dirMod.go
+++ createAndDeleteDir/dirMod.go
@@ -10,46 +10,6 @@ import (
 var savings string = "savings"
 var current string = "current"
 
-func CreateSavingsAndCurrentDirectoriesToStoreTheDB(){
-    if _, err := os.Stat(savings); os.IsNotExist(err){
-        err1 := os.MkdirAll(savings, 0755)
-        if err1 != nil{
-            fmt.Println("While creating an savings & an current directory account we found : ", err1)
-            return
-        }
-        fmt.Println("Created an savings account successfully")
-    }else{
-        fmt.Println("savings account already exists")
-    }
-
-
-    if _, err := os.Stat(current); os.IsNotExist(err){
-        err2 := os.MkdirAll(current, 0755)
-        if err2 != nil{
-            fmt.Println("While creating an savings & an current directory account we found : ", err2)
-            return
-        }
-        fmt.Println("Created an current account successfully")
-    }else{
-        fmt.Println("current account already exists")
-    }
-}
-
-func CreateSavingsAndCurrentFilesToStoreTheDB(){
-    createFile := filepath.Join(savings, "savingsAccountDB.json")
-    file1, err1 := os.Create(createFile)
-    createFile = filepath.Join(current, "currentAccountDB.json")
-    file2, err2 := os.Create(createFile)
-    err := errors.Join(err1, err2)
-    if err != nil{
-        fmt.Println("while creating an savings and current file we found : ", err)
-        return
-    }
-    defer file1.Close()
-    defer file2.Close()
-    fmt.Println("Created files successfully")
-}
-
 func CreateSavingsDirectoriesToStoreTheDB(){
     if _, err := os.Stat(savings); os.IsNotExist(err){
         err1 := os.MkdirAll(savings, 0755)
@@ -73,4 +33,29 @@ func CreateSavingsFilesToStoreTheDB(){
     }
     defer file.Close()
     fmt.Println("Created savings account files successfully")
+}
+
+
+func CreateCurrentDirectoriesToStoreTheDB(){
+    if _, err := os.Stat(current); os.IsNotExist(err){
+        err2 := os.MkdirAll(current, 0755)
+        if err2 != nil{
+            fmt.Println("While creating an savings & an current directory account we found : ", err2)
+            return
+        }
+        fmt.Println("Created an current account successfully")
+    }else{
+        fmt.Println("current account already exists")
+    }
+}
+
+func CreateCurrentFilesToStoreTheDB(){
+    createFile = filepath.Join(current, "currentAccountDB.json")
+    file, err := os.Create(createFile)
+    if err != nil{
+        fmt.Println("while creating an savings and current file we found : ", err)
+        return
+    }
+    defer file.Close()
+    fmt.Println("Created current account files successfully")
 }
