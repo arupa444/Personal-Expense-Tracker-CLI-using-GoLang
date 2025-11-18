d85090519aa2b99988ef4c314d140d65c67c1df3
arupanandaswain <arupaswain7735@gmail.com>
2025-11-18 11:51:31 +0530

    updated main and dirMod files

--- createAndDeleteDir/dirMod.go
+++ createAndDeleteDir/dirMod.go
@@ -6,9 +6,12 @@ import (
     "errors"
 )
 
+var savings string = "savings"
+var current string = "current"
+
 func CreateSavingsAndCurrentDirectoriesToStoreTheDB(){
-    err1 := os.Mkdir("saving", 0755)
-    err2 := os.Mkdir("current", 0755)
+    err1 := os.MkdirAll(savings, 0755)
+    err2 := os.MkdirAll(current, 0755)
     err := errors.Join(err1, err2)
     if err != nil{
         fmt.Println("While creating an savings & an current account we found : ", err)
