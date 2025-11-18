0546e5405783533fcf20e24545a4f12de13db948
arupanandaswain <arupaswain7735@gmail.com>
2025-11-18 11:43:55 +0530

    updated main and dirMod files

--- main.go
+++ main.go
@@ -1,21 +1,6 @@
 package main
 
-import (
-    "fmt"
-    "os"
-    "errors"
-)
 
-func createSavingsAndCurrentDirectoriesToStoreTheDB(){
-    err1 := os.Mkdir("saving", 0755)
-    err2 := os.Mkdir("current", 0755)
-    err := errors.Join(err1, err2)
-    if err != nil{
-        fmt.Println("While creating an savings & an current account we found : ", err)
-        return
-    }
-    fmt.Println("Created an savings and an current account")
-}
 
 func main(){
     createSavingsAndCurrentDirectoriesToStoreTheDB();
