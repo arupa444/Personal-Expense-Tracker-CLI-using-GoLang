bc002145ae6e2f144cb564f85def1dd7a5ae104d
arupanandaswain <arupaswain7735@gmail.com>
2025-11-20 11:06:40 +0530

    updated main and dirMod files

--- main.go
+++ main.go
@@ -5,4 +5,14 @@ import (
 )
 
 func main(){
+
+// //     To create the saving account dir
+//     dirMod.CreateSavingsDirectoriesToStoreTheDB()
+// //     To create the saving account file in the dir
+//     dirMod.CreateSavingsFilesToStoreTheDB()
+// //     To create the current account dir
+//     dirMod.CreateCurrentDirectoriesToStoreTheDB()
+// //     To create the current account file in the dir
+//     dirMod.CreateCurrentFilesToStoreTheDB()
+
 }
